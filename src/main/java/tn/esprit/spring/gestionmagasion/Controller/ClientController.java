fb2b435d369b4ef1b4388e04c6a50226a21612e9
Firas GACHA <firas.gacha@esprit.tn>
2021-11-09 19:27:58 +0100

    Gestion Magasin+REST+Swagger

--- src/main/java/tn/esprit/spring/gestionmagasion/Controller/ClientController.java
+++ src/main/java/tn/esprit/spring/gestionmagasion/Controller/ClientController.java
@@ -1,6 +1,8 @@
 package tn.esprit.spring.gestionmagasion.Controller;
 
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -13,31 +15,37 @@ import java.util.List;
 @RestController
 @CrossOrigin("*")
 @RequestMapping("/client/")
+@Api(tags = "Client management")
 public class ClientController  {
 
     @Autowired
     private ClientService clientService;
 
+    @ApiOperation(value = "Ajouter un Client")
     @PostMapping("add")
     public Client add(@RequestBody Client client) {
         return clientService.add(client);
     }
 
+    @ApiOperation(value = "Mettre a jour un Client")
     @PutMapping("update/{id}")
     public Client update(@Valid @RequestBody Client client,@PathVariable("id") Long id) {
         return clientService.update(client, id);
     }
 
+    @ApiOperation(value = "Supprimer un Client")
     @DeleteMapping("delete/{id}")
     public void delete(@PathVariable("id") long id) {
         clientService.delete(id);
     }
 
+    @ApiOperation(value = "Afficher la liste des Client")
     @GetMapping("list")
     public List<Client> findAll() {
         return clientService.findAll();
     }
 
+    @ApiOperation(value = "Chercher un Client par son ID")
     @GetMapping("findById/{id}")
     public Client findById(@PathVariable("id") Long id) {
         return clientService.findById(id);
--- src/main/java/tn/esprit/spring/gestionmagasion/GestionMagasionApplication.java
+++ src/main/java/tn/esprit/spring/gestionmagasion/GestionMagasionApplication.java
@@ -3,9 +3,11 @@ package tn.esprit.spring.gestionmagasion;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.List;
 
+@EnableSwagger2
 @SpringBootApplication
 @Slf4j
 public class GestionMagasionApplication {
